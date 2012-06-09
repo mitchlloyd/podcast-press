ee3dbfd1169756b7ffcbfa790c55025cadcf7e1f
Mitch Lloyd <mitch.lloyd@gmail.com>
2012-06-09 12:32:19 -0400

    Setting the episode_number sets the track number.

--- lib/podcast_press/audio_file.rb
+++ lib/podcast_press/audio_file.rb
@@ -1,23 +1,37 @@
 require 'taglib'
+require 'ostruct'
+require 'forwardable'
 
 module PodcastPress
   class AudioFile
+    extend Forwardable
+
+    def_delegators :@params, :title
+
     def initialize(filename)
       @filename = filename
     end
 
     def tag!(params={})
-      @params = params
+      @params = OpenStruct.new(params)
 
       TagLib::MPEG::File.open(@filename) do |file|
         tag = file.id3v2_tag
-        tag.title = title
+
+        # Setting a nil values causes a segfaults and other errors in the talib library
+        tag.title = title if title
+        tag.track = episode_number if episode_number
+
         file.save
       end
     end
 
-    def title
-      @params[:title]
+    def episode_number(options={})
+      if (padding_amount = options[:padding])
+        "%0#{padding_amount}d" % @params.episode_number
+      else
+        @params.episode_number
+      end
     end
   end
 end
--- specs/audio_file_spec.rb
+++ specs/audio_file_spec.rb
@@ -7,14 +7,17 @@ require 'podcast_press'
 FILENAME = './sandbox/test_file.mp3'
 
 describe PodcastPress do
+  before do
+    @file = File.new(FILENAME, 'w')
+  end
+
+  after do
+    File.delete(@file.path)
+  end
+
   describe "when #press! is called with a title" do
     before do
-      @file = File.new(FILENAME, 'w')
       @episode = PodcastPress.press!(@file.path, title: 'Sweet Episode')
-    end
-
-    after do
-      File.delete(@file.path)
     end
 
     it "returns the title when #title is called" do
@@ -24,6 +27,22 @@ describe PodcastPress do
     it "sets the title ID3 tag to the given title" do
       TagLib::FileRef.open(@file.path) do |file|
         file.tag.title.must_equal 'Sweet Episode'
+      end
+    end
+  end
+
+  describe "when #press! is called with episode_number" do
+    before do
+      @episode = PodcastPress.press!(@file.path, episode_number: 1)
+    end
+
+    it "returns the episode_number with padding" do
+      @episode.episode_number(padding: 3).must_equal '001'
+    end
+
+    it "sets the track number tag" do
+      TagLib::FileRef.open(@file.path) do |file|
+          file.tag.track.must_equal 1
       end
     end
   end
